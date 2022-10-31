20681132d0d81ef6d55ceb45cb34f97e4704ca93
Heewon Lee <ncross42@gmail.com>
2022-10-31 09:49:06 +0900

    change title to 'test-cagfe'

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '집pc의 페이지' });
+  res.render('index', { title: 'cafe의 테스트 페이지' });
 });
 
 module.exports = router;
